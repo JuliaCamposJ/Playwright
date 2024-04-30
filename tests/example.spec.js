1e5c8f6455061937e83f308c7d8183846dc64f56
JuliaCamposJ <105725139+JuliaCamposJ@users.noreply.github.com>
2024-04-30 20:46:02 -0300

    Alt Arquivos

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,13 +19,3 @@ test('Login com sucesso', async ({ page }) => {
   //acima está o print de tela.
   await page.getByRole('button', { name: 'login' }).click();
 });
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
