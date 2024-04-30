7b177be83412181dcce6210b71a1e829f0d67ff0
JuliaCamposJ <105725139+JuliaCamposJ@users.noreply.github.com>
2024-04-30 20:16:52 -0300

    VSPlay

---
+++ tests/example.spec.js
@@ -0,0 +1,32 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.use({
+  viewport: { width: 1600, height: 1200 },
+});
+// isso acima define o tamanho da tela a resolução
+test('Login com sucesso', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
+  await page.getByRole('link', { name: ' Login' }).click();
+  //await page.screenshot({ path: 'screenshot/screenshot.png' });
+  //esse acima cria a pasta e guarda o print nela, se usado várias vezes não vai criar apenas guardar.
+  await page.locator('#user').click();
+  await page.locator('#user').fill('teste@gmail.com');
+  await page.locator('#password').click();
+  //await page.locator('#password').screenshot({ path: 'screenshot/elementosenha.png' });
+  //esse tira print do elemento especifico e guarda.
+  await page.locator('#password').fill('123456');
+  //await page.screenshot({ path: 'screenshot.png' });
+  //acima está o print de tela.
+  await page.getByRole('button', { name: 'login' }).click();
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
---
+++ tests/testegeo.spec.js
@@ -0,0 +1,8 @@
+// @ts-check
+//const { test, expect } = require('@playwright/test');
+//
+//test.use({
+  //  geolocation: { longitude: 41.890221, latitude: 12.492348 },
+  //  permissions: ['geolocation'],
+  //});
+// isso acima define a geolocalização
