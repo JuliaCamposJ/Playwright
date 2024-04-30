abfc238fa29795c6353378cf527e4c481e687d37
JuliaCamposJ <105725139+JuliaCamposJ@users.noreply.github.com>
2024-04-30 20:38:55 -0300

    Teste

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/search?q=google&oq=google&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDQ3MjdqMGoyqAIAsAIA&sourceid=chrome&ie=UTF-8');
-  await page.getByRole('link', { name: 'Google Google https://www.google.com.br ›' }).click();
-  await page.getByLabel('Pesquisar', { exact: true }).click();
-  await page.getByLabel('Pesquisar', { exact: true }).fill('teste de velocidade');
-});
+//import { test, expect } from '@playwright/test';
+//
+//test('test', async ({ page }) => {
+  //await page.goto('https://www.google.com/search?q=google&oq=google&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDQ3MjdqMGoyqAIAsAIA&sourceid=chrome&ie=UTF-8');
+  //await page.getByRole('link', { name: 'Google Google https://www.google.com.br ›' }).click();
+  //await page.getByLabel('Pesquisar', { exact: true }).click();
+  //await page.getByLabel('Pesquisar', { exact: true }).fill('teste de velocidade');
+//});
